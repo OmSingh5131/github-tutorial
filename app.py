c7e8f110d268148b734a2ade692950a70c242535
OmSingh5131 <154455357+OmSingh5131@users.noreply.github.com>
2025-09-22 17:34:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -121,4 +121,6 @@ with gr.Blocks() as demo:
     )
 
 if __name__ == "__main__":
-    demo.launch(share = True)
+    port = int(os.environ.get("PORT", 7860))  # Render sets PORT env var
+    demo.launch(server_name="0.0.0.0", server_port=port)
+
